590401b6eca8f1e04108eadca88ee0991b5b9083
DRANetGH35 <144178956+DRANetGH35@users.noreply.github.com>
2025-10-29 12:48:47 -0700

    add: game logic

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-One, Two, Three, Four, Five, Six, Seven, Eight, Nine = 1, 2, 3, 4, 5, 6, 7, 8, 9
+from sys import excepthook
 
-board = f""" ___ ___ ___
-| {One} | {Two} | {Three} |
+P = {
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9
+}
+board = {}
+def refresh_board():
+    global board
+    board = f""" ___ ___ ___
+| {P[1]} | {P[2]} | {P[3]} |
 |___|___|___|
-| {Four} | {Five} | {Six} |
+| {P[4]} | {P[5]} | {P[6]} |
 |___|___|___|
-| {Seven} | {Eight} | {Nine} |
+| {P[7]} | {P[8]} | {P[9]} |
 |___|___|___|"""
+
+
+
+while True:
+    refresh_board()
+    print(board)
+    try:
+        User_Input = int(input("Please select a number from the board to place an X"))
+        P[User_Input] = 'X'
+    except ValueError:
+        print("Please enter a number 1-10")
+
 
