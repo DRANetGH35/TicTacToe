9f8acc82f3de7e1514a9b417d53eb440adc4e740
DRANetGH35 <144178956+DRANetGH35@users.noreply.github.com>
2025-10-29 12:22:38 -0700

    add: board

---
+++ main.py
@@ -0,0 +1,10 @@
+One, Two, Three, Four, Five, Six, Seven, Eight, Nine = 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+board = f""" ___ ___ ___
+| {One} | {Two} | {Three} |
+|___|___|___|
+| {Four} | {Five} | {Six} |
+|___|___|___|
+| {Seven} | {Eight} | {Nine} |
+|___|___|___|"""
+
